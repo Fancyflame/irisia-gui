801ed9a48425511aff417c48a1e422803562fb6b
fancyflame <fancyflame@163.com>
2025-08-11 14:31:25 +0800

    chore(irisia-macros): 组件语句现在会主动检查属性冲突

--- irisia-macros/src/component/parse/use_component.rs
+++ irisia-macros/src/component/parse/use_component.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::component::{ast::*, parse::parse_stmts};
 use syn::{
     Error, Ident, Result, Token, braced, bracketed, parse::ParseStream, spanned::Spanned,
@@ -22,20 +24,30 @@ pub fn parse_component(input: ParseStream) -> Result<ComponentStmt> {
     braced!(content in input);
 
     let mut fields = Vec::new();
+    let mut field_pool = HashSet::new();
     let mut child_data = None;
 
     while let Some(fa) = parse_field_assignment(&content)? {
         match fa.name {
-            FieldAssignmentName::Ident(ident) => fields.push(FieldAssignment {
-                name: ident,
-                value: fa.value,
-                decoration: fa.decoration,
-            }),
+            FieldAssignmentName::Ident(ident) => {
+                if !field_pool.insert(ident.to_string()) {
+                    return Err(Error::new(
+                        ident.span(),
+                        format!("field `{ident}` already exists"),
+                    ));
+                }
+
+                fields.push(FieldAssignment {
+                    name: ident,
+                    value: fa.value,
+                    decoration: fa.decoration,
+                })
+            }
             FieldAssignmentName::Super(super_token) => {
                 if child_data.is_some() {
                     return Err(Error::new_spanned(
                         super_token,
-                        "cannot define child data duplicatedly",
+                        "cannot define `super` property duplicatedly",
                     ));
                 }
                 if !matches!(fa.decoration, FieldDecoration::None) {
@@ -49,11 +61,21 @@ pub fn parse_component(input: ParseStream) -> Result<ComponentStmt> {
         }
     }
 
+    let body_span = content.span();
+    let body = parse_stmts(&content)?;
+
+    if !body.is_empty() && field_pool.contains("children") {
+        return Err(Error::new(
+            body_span,
+            "cannot define child elements because `children` property has been defined manually",
+        ));
+    }
+
     Ok(ComponentStmt {
         comp_type,
         child_data,
         fields,
-        body: parse_stmts(&content)?,
+        body,
     })
 }
 
